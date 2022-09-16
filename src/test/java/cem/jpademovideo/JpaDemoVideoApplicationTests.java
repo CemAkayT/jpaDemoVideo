788471c1392f38dfa71c2fbc0de91ca81a790222
cemakay <cema0092@stud.kea.dk>
2022-09-16 10:44:11 +0200

    commit 16/9

--- src/test/java/cem/jpademovideo/JpaDemoVideoApplicationTests.java
+++ src/test/java/cem/jpademovideo/JpaDemoVideoApplicationTests.java
@@ -3,6 +3,7 @@ package cem.jpademovideo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/*
 @SpringBootTest
 class JpaDemoVideoApplicationTests {
 
@@ -11,3 +12,4 @@ class JpaDemoVideoApplicationTests {
     }
 
 }
+*/
